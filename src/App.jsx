d73b2ba3ca288aa1f0e2d54efec85fbc3539f21a
priyanshuu23 <priyanshu.urkade.lecs21@ggits.net>
2024-02-20 00:26:20 +0530

    Add react-type-animation package and update Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     // Simulate loading time
     setTimeout(() => {
       setLoading(false); // Once loading is done, set loading to false
-    }, 3000); // Adjust the delay as needed
+    }, 300); // Adjust the delay as needed
   }, []);
 
   const handleMouseEnter = () => {
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,18 +2,50 @@ import React from "react";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
+import { TypeAnimation } from 'react-type-animation';
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Priyanshu </h1> 
+
+      <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.aTag}>
+        <TypeAnimation className={styles.title} 
+        
+            sequence={[
+              "Hi,",
+              100,
+              "Hi, I'm Priyanshu",
+              500,
+            ]}
+            speed={50}
+            repeat={false} /> </a>
+
+
+      <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.aTag}>
+        <TypeAnimation className={styles.info}  
+           style={{ whiteSpace: 'pre-line', height: '40px', display: 'block' }}
+              sequence={[
+                2000,
+               "Welcome to my digital playground!",
+              900,
+              "where creativity meets strategy!",
+              900,
+              "I design experiences that captivate users and drive results.",
+              900,
+              "I turn complex ideas into user-friendly solutions.",
+              900,
+              "Ready to elevate your brand? Let's chat!",
+              900,
+              ]}   
+              speed={75}
+            repeat={false} />   </a>
+
+
+        {/* <h1 className={styles.title}>Hi, I'm Priyanshu </h1>  */}
         {/* <span className="blog-title-emoji">👋</span> */}
-        <p className={styles.description}>
-           I am a aspiring Full-Stack developer in the process of learning Computer Science & Engineering at GGITS.
-           Exploring MERN Stack for crafting WebApps and Websites. Actively working on honing skills 
-           through independent projects. 🚀💻
-        </p>
+        <p className={styles.description}>I am a aspiring Full-Stack developer in the process of learning Computer Science & Engineering at GGITS. Exploring MERN Stack for crafting WebApps and Websites. Actively working on honing skills through independent projects. 🚀💻 </p>
+
         <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.contactBtn}>
           Contact Me
         </a>
