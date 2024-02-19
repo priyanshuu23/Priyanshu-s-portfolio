d5ed6120474e77bc04fc3991845ad4fc67b7565e
priyanshuu23 <priyanshu.urkade.lecs21@ggits.net>
2024-02-20 01:29:25 +0530

    Update Hero component with new content and styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,29 +22,40 @@ export const Hero = () => {
             repeat={false} /> </a>
 
 
-      <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.aTag}>
+        {/* <h1 className={styles.title}>Hi, I'm Priyanshu </h1>  */}
+        {/* <span className="blog-title-emoji">👋</span> */}
+        <p className={styles.description}>I am a aspiring Full-Stack developer in the process of learning Computer Science & Engineering at GGITS. Exploring MERN Stack for crafting WebApps and Websites. Actively working on honing skills through independent projects. 🚀💻
+
+        <br /> 
+        
+        
+        <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.aTag}>
         <TypeAnimation className={styles.info}  
-           style={{ whiteSpace: 'pre-line', height: '40px', display: 'block' }}
+            
               sequence={[
                 2000,
                "Welcome to my digital playground!",
               900,
               "where creativity meets strategy!",
               900,
-              "I design experiences that captivate users and drive results.",
-              900,
-              "I turn complex ideas into user-friendly solutions.",
-              900,
-              "Ready to elevate your brand? Let's chat!",
+              "Ready to colaborate? Let's chat!",
               900,
               ]}   
               speed={75}
             repeat={false} />   </a>
+              <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.aTag}>
+        <TypeAnimation className={styles.infoo}  
+            style={{ fontSize: '0.8em' }}
+              sequence={[
+                2000,
+              "Ready to colaborate? Let's chat!",
+              900,
+              ]}   
+              speed={75}
+            repeat={false} />   </a>    
+        
+        </p>
 
-
-        {/* <h1 className={styles.title}>Hi, I'm Priyanshu </h1>  */}
-        {/* <span className="blog-title-emoji">👋</span> */}
-        <p className={styles.description}>I am a aspiring Full-Stack developer in the process of learning Computer Science & Engineering at GGITS. Exploring MERN Stack for crafting WebApps and Websites. Actively working on honing skills through independent projects. 🚀💻 </p>
 
         <a href="mailto:xyz@gmail.com?subject=Hello&body=Hello" target="_blank" className={styles.contactBtn}>
           Contact Me
