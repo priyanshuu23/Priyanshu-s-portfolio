203d25386e25aab39423ce094e4e3478fbe24374
priyanshuu23 <priyanshu.urkade.lecs21@ggits.net>
2024-03-13 22:15:59 +0530

    Refactor ProjectCard component

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
+
+
+
 export const ProjectCard = ({
   project: { title, imageSrc, description, skills, demo, source },
 }) => {
   return (
+  
     <div className={styles.container}>
       <img
         src={getImageUrl(imageSrc)}
